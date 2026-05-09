7758ed8b3ed49fab2b01137c227fdb2005af206e
Thommy Schonis <info@lynqagency.com>
2026-05-09 13:32:56 +0200

    feat: install Sentry error tracking

---
+++ instrumentation-client.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8e494edd541ddd5c5c940d205fdbddff@o4511359613140992.ingest.de.sentry.io/4511359620350032",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
